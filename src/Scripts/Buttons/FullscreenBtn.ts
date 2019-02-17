60c2cbba0f83036f89875c05f3b7dc4d0642a22b
Martin Král <mail2maras@gmail.com>
2019-02-17 22:55:12 +0100

    New fullscreen

--- src/Scripts/Buttons/FullscreenBtn.ts
+++ src/Scripts/Buttons/FullscreenBtn.ts
@@ -2,12 +2,8 @@ import { ForcedButton } from "./ForcedButton";
 
 export class FullscreenBtn extends ForcedButton {
     private scene: Phaser.Scene;
-    private canvas: HTMLCanvasElement;
-    private fullscreen: Phaser.Device.Fullscreen;
     constructor(posX: number, posY: number, btnGraphicsName: string, scene: Phaser.Scene) {
         super(posX, posY, btnGraphicsName, scene);
-        this.canvas = scene.sys.game.canvas;
-        this.fullscreen = scene.sys.game.device.fullscreen;
         this.scene = scene;
     }
 
